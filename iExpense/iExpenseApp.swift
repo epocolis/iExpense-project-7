ff61dfecfd9434854d374f59168a8c1aa7c6292a
leotis <mekdemcrossit@tutanota.com>
2021-04-23 18:25:05 -0400

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Leotis buchanan on 2021-04-23.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
