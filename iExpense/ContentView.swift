7b7443454db7e60652ce094991f00b34da13f32c
leotis <mekdemcrossit@tutanota.com>
2021-04-23 19:30:52 -0400

    deleting itens using onDelete()

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -6,63 +6,50 @@
 //
 /*
  
- There are several ways of showing views in SwiftUI, and one of the most basic is a sheet:
- a new view presented on top of our existing one. On iOS this automatically gives us a card-like
- presentation where the current view slides away into the distance a little and the new view animates in on top.
-
- Sheets work much like alerts, in that we don’t present them directly with code such as mySheet.present() or similar.
- Instead, we define the conditions under which a sheet should be shown, and when those conditions become true or
- false the sheet will either be presented or dismissed respectively.
-
- Let’s start with a simple example, which will be showing one view from another using a sheet.
- First, we create the view we want to show inside a sheet, like this:
+ SwiftUI gives us the onDelete() modifier for us to use to control
+ how objects should be deleted from a collection.
+ In practice, this is almost exclusively used with List and ForEach: we
+ create a list of rows that are shown using ForEach, then attach onDelete()
+ to that ForEach so the user can remove rows they don’t want.
+ 
+ This is another place where SwiftUI does a heck of a lot of work on our behalf,
+ but it does have a few interesting quirks as you’ll see.
+ 
+ First, let’s construct an example we can work with: a list that shows numbers,
+ and every time we tap the button a new number appears. Here’s the code for that:
  
  
  */
 
 import SwiftUI
 
-
-
-struct SecondView: View {
-    
-    @Environment(\.presentationMode) var presentationMode
-    
-    var name: String
-    
-    var body : some View {
-        
-        VStack {
-            
-            Text("Second View")
-            
-            Button("Dismiss") {
-                self.presentationMode.wrappedValue.dismiss()
-            }
-            
-        }
-        
-       
-    }
-}
-
-
-
-
 struct ContentView: View {
     
-  @State private var showingSheet = false
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
     
+    
+    func removeRows(at offsets: IndexSet){
+        numbers.remove(atOffsets: offsets)
+    }
+ 
     var body: some View {
-    
-        Button("Show Sheet"){
-            // show the sheet
-            self.showingSheet.toggle()
+        NavigationView{
+            VStack {
+                List {
+                    ForEach(numbers, id:\.self ) {
+                        Text("\($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number"){
+                    self.numbers.append(self.currentNumber)
+                    self.currentNumber += 1
+                    
+                }
+            }.navigationBarItems(leading: EditButton())
         }
-        .sheet(isPresented: $showingSheet){
-            SecondView(name: "@twostraws")
-        }
-        
         
     }
 }
