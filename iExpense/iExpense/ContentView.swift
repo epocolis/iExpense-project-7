aa6389bdb8cce8123a6c83d485056bbda11af610
leotis <mekdemcrossit@tutanota.com>
2021-04-27 07:23:05 -0400

    How to hide views
    This code show you how to hide views see ContentView.swift

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -4,16 +4,36 @@
 //
 //  Created by Leotis buchanan on 2021-04-27.
 //
+/*
+ SwiftUI gives us the onDelete() modifier for us to use to control how
+ objects should be deleted from a collection. In practice, this is
+ almost exclusively used with List and ForEach: we create a list of
+ rows that are shown using ForEach, then attach onDelete() to that
+ ForEach so the user can remove rows they don’t want.
+
+ This is another place where SwiftUI does a heck of a lot of work on
+ our behalf, but it does have a few interesting quirks as you’ll see.
+ 
+ */
 
 import SwiftUI
 
 
 struct SecondView: View {
     
+    @Environment(\.presentationMode) var presentationMode
+    
     var name:String
     
     var body: some View {
-        Text("Second View hello from \(name)")
+       
+        VStack {
+            Text("Second Sheet, hello from \(name)")
+            Button("Dismiss") {
+                self.presentationMode.wrappedValue.dismiss()
+            }
+        }
+    
     }
 }
 
